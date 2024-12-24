f0568e33904f52f7f73e7ba97dd9432ddf54b50c
Sulton <sultonalidrus5@gmail.com>
2024-12-24 20:52:26 +0700

    feat : add home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,5 @@
-import { DemoCrop } from "@/components/ui/demo-crop";
 import { Metadata } from "next";
+import Image from "next/image";
 import React from "react";
 
 export const dynamic = "force-dynamic";
@@ -11,7 +11,10 @@ export const metadata: Metadata = {
 const Page = () => {
   return (
     <div className="grid min-h-screen place-items-center">
-      <DemoCrop />
+      <div className="flex flex-col items-center justify-center gap-2">
+        <Image src="/chat.png" alt="chat" width={500} height={500} priority />
+        <p>Chatting with your friends</p>
+      </div>
     </div>
   );
 };
